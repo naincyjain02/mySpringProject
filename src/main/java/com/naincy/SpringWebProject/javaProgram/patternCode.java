6c6b5573e34cdf18d8f57bb586b3b03f5f527cc2
Naincyj <naincyjain654321@gmail.com>
2025-04-24 11:41:50 +0530

    Code written for Pattern present in patternCode.java file

---
+++ src/main/java/com/naincy/SpringWebProject/javaProgram/patternCode.java
@@ -0,0 +1,35 @@
+package com.naincy.SpringWebProject.javaProgram;
+
+public class patternCode {
+  public static void main(String[] args) {
+	  int m=6;
+	  int n=11;
+	  for(int i=0;i<m;i++) {
+		  for(int j=0;j<n;j++) {
+			  if(i==0||i==(m-1)) {
+				  System.out.print("* ");
+			  }
+			  else {
+				  if(j==0||j==(n-1)) {
+					  System.out.print("* ");
+				  }
+				  else {
+					  if(j<=(n/2+1-i)-1)
+						  System.out.print("  ");
+					  else if(j>=(n/2+1+(2*i-1)/2)&& j<(n-1))
+						  System.out.print("  ");
+					  else
+						  System.out.print("* ");
+				  }
+			  }
+		  }
+		  System.out.println();
+	  }
+	  int a=(m-2)*(m-2);
+	   int b=a+2*m+2*(n-2);
+	  System.out.println("Stars in Triangle are :"+a);
+	  System.out.println("Stars in Rectangle are :"+b);
+	  
+  }
+}
+
